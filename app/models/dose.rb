2dba44e4ff9ae3172d5eb288c1103866cae83ab9
hamze awawde <ham.awawde@gmail.com>
2019-11-21 12:21:46 +0200

    small change

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,6 +1,5 @@
 class Dose < ApplicationRecord
-  belongs_to :ingredient
   belongs_to :cocktail
-  validates :description, presence: true
-  validates_uniqueness_of :ingredient_id, :scope =>[:cocktail_id]
+  belongs_to :ingredient
+  validates :cocktail, uniqueness: { scope: :ingredient }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,15 @@
-require 'json'
 require 'open-uri'
 
-Ingredient.destroy_all
-puts "removed ingredients from the list"
+puts "Destroy ingredients"
+Ingredient.destroy_all if Rails.env.development?
 
-url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
-string = open(url).read
-hash = JSON.parse(string)
-array = hash['drinks']
-array.each do |minihash|
-  ingredient = minihash['strIngredient1']
-  Ingredient.create(name:ingredient)
-  puts "created another #{ingredient}"
+puts "Destroy Cocktails"
+Cocktail.destroy_all if Rails.env.development?
+
+puts "Create ingredients"
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+ingredients = JSON.parse(open(url).read)
+ingredients["drinks"].each do |ingredient|
+  i = Ingredient.create(name: ingredient["strIngredient1"])
+  puts "create #{i.name}"
 end
